2dd9bfba96fafd1d57f567bfd2a4b553f19127b0
vishalmeena2211 <meenavishal2211@gmail.com>
2024-05-26 16:27:46 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+
+    <div className='flex justify-center items-center h-screen w-screen'>
+
+    <Button>Vishal Meena</Button>
+    </div>
+
+      
+    </>
+  )
+}
+
+export default App
